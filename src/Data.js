861c37a4a7e49c9ac817afd094bf0b8dd00b4699
sugiwa <sugi.warra@gmail.com>
2020-09-26 16:48:01 +0900

    second commit

--- src/Data.js
+++ src/Data.js
@@ -19,7 +19,7 @@ function Data() {
             description:'Reactを用いて作成したポートフォリオで、Netlifyにデプロイしました。',
             image:portfolio,
             url:'https://distracted-bassi-d1ec6e.netlify.app',
-            details:'ポートフォリオサイトをまだ持っていなかったのと、Reactの技術をより深めたいと思い作成しました。レスポンシブで白を基調としたシンプルなデザインを意識しました。',
+            details:'ポートフォリオサイトをまだ持っておらず、Reactの技術をより深めたいと思い作成しました。レスポンシブで白を基調としたシンプルなデザインを意識しました。',
             skill:'React'
         },
     ]
